5cb78ba9af472ceafb7a16316e5ddf4b5e5d313c
Paarthkumar26 <paarthkumar199@gmail.com>
2026-02-10 23:19:35 +0530

    init

--- 2 ps/06_prbl.py
+++ 2 ps/06_prbl.py
@@ -3,4 +3,4 @@ a = int(input("Enter your number 1: "))
 
 
 print("the square of the number is", a**2)
-#print("the square of the number is", a^2) ^ not valid in pyhton 
+#print("the square of the number is", a^2) ^ not valid in pyhton qqq
