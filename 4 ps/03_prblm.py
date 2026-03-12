6125b211b858accdb31500833d88a4cc66d3d37f
Paarthkumar26 <paarthkumar199@gmail.com>
2026-03-12 23:37:08 +0530

    12/3/26

---
+++ 4 ps/03_prblm.py
@@ -0,0 +1,2 @@
+balle = (33 ,444, "paarth" , 3.14)
+ a[2] = "paarthkumar"
--- 4/05_tupel_methods.py
+++ 4/05_tupel_methods.py
@@ -2,3 +2,9 @@ a = (1,2 , False , "paarth" , 3.14)
 print(a)
 print(type(a))
 
+no = a.count (45)
+print(no)
+
+
+i = a.index (2)
+print(i)
